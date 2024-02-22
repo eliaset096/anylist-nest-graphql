ea2ac71a690414cd3f228dc079d1980a1ae109e9
Elías Estupiñan <Elias.Estupinan@carvajal.com>
2024-02-22 01:05:43 -0500

    Add .env.templete, .env, and /postgres to .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,40 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { envLoaderConfig } from './common/config/env-loader.config';
+import { envValidationSchema } from './common/config/env-validation-schema.config';
+import { ItemsModule } from './items/items.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [envLoaderConfig],
+      validationSchema: envValidationSchema,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true, // only for development
+      autoLoadEntities: true,
+    }),
+
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/items/items.resolver.ts
@@ -0,0 +1,35 @@
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateItemInput } from './dtos/inputs/create-item.input';
+import { UpdateItemInput } from './dtos/inputs/update-item.input';
+import { Item } from './entities/item.entity';
+import { ItemsService } from './items.service';
+
+@Resolver(() => Item)
+export class ItemsResolver {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Mutation(() => Item)
+  createItem(@Args('createItemInput') createItemInput: CreateItemInput) {
+    return this.itemsService.create(createItemInput);
+  }
+
+  @Query(() => [Item], { name: 'items' })
+  findAll() {
+    return this.itemsService.findAll();
+  }
+
+  @Query(() => Item, { name: 'item' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.itemsService.findOne(id);
+  }
+
+  @Mutation(() => Item)
+  updateItem(@Args('updateItemInput') updateItemInput: UpdateItemInput) {
+    return this.itemsService.update(updateItemInput.id, updateItemInput);
+  }
+
+  @Mutation(() => Item)
+  removeItem(@Args('id', { type: () => Int }) id: number) {
+    return this.itemsService.remove(id);
+  }
+}
