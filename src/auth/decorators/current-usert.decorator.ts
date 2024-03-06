34f8b617c5fd54912bce274e0e1d06c8092d5290
Elías Estupiñan <Elias.Estupinan@carvajal.com>
2024-03-05 23:35:02 -0500

    Add support for develop branch in CI workflow and update CurrentUser decorator documentation

--- src/auth/decorators/current-usert.decorator.ts
+++ src/auth/decorators/current-usert.decorator.ts
@@ -8,6 +8,15 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/users/entities/user.entity';
 import { ValidRoles } from '../enums/valid-roles.enum';
 
+/**
+ * Decorator that retrieves the current user from the request context.
+ * Throws an error if the user is not found or if the user does not have the necessary permissions.
+ * @param roles - An array of valid roles that the user must have. If empty, no role check is performed.
+ * @param context - The execution context of the current request.
+ * @returns The current user if found and has the necessary permissions.
+ * @throws InternalServerErrorException - If the user is not found in the request context.
+ * @throws ForbiddenException - If the user does not have the necessary permissions.
+ */
 export const CurrentUser = createParamDecorator(
   (roles: ValidRoles[] = [], context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
